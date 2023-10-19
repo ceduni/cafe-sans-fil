124f1958d15418b09b76ca261d8f97a84cab4683
axeelz <axelzareb@hotmail.fr>
2023-10-19 17:59:40 -0400

    ajout Opening Hours

---
+++ front/src/components/ui/OpeningHours.jsx
@@ -0,0 +1,71 @@
+const OpeningHours = ({
+  openingHours = [
+    {
+      day: "lundi",
+      blocks: [
+        { start: "08:00", end: "12:00" },
+        { start: "14:00", end: "18:00" },
+      ],
+    },
+    {
+      day: "mardi",
+      blocks: [
+        { start: "08:00", end: "12:00" },
+        { start: "14:00", end: "18:00" },
+      ],
+    },
+    {
+      day: "mercredi",
+      blocks: [
+        { start: "08:00", end: "12:00" },
+        { start: "14:00", end: "18:00" },
+      ],
+    },
+    {
+      day: "jeudi",
+      blocks: [
+        { start: "08:00", end: "12:00" },
+        { start: "14:00", end: "18:00" },
+      ],
+    },
+    {
+      day: "vendredi",
+      blocks: [{ start: "08:00", end: "18:00" }],
+    },
+    {
+      day: "samedi",
+      blocks: [],
+    },
+    {
+      day: "dimanche",
+      blocks: [],
+    },
+  ],
+} = {}) => {
+  // Ce composant prend un objet de type
+  // [{ "day": "string", "blocks": [{"start": "string (HH:mm format)", "end": "string (HH:mm format)" }] }]
+  // et le transforme en une liste d'éléments HTML avec chaque jour et ses horaires.
+
+  return (
+    <div className="mt-10">
+      <h2 className="text-2xl font-bold tracking-tight text-gray-900">Horaires d'ouverture</h2>
+      <div className="mt-6 gap-4 md:gap-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {openingHours.map((day) => (
+          <div key={day.day}>
+            <h3 className="text-lg font-semibold text-gray-900 capitalize">{day.day}</h3>
+            <ul className="mt-3 text-base text-gray-500">
+              {day.blocks.length === 0 && <li>Fermé</li>}
+              {day.blocks.map((block) => (
+                <li key={block.start}>
+                  {block.start}&nbsp;-&nbsp;{block.end}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OpeningHours;
--- front/src/routes/Cafe.jsx
+++ front/src/routes/Cafe.jsx
@@ -3,6 +3,7 @@ import ItemCard from "../components/ui/ItemCard";
 import Container from "../components/ui/Container";
 import { products } from "../components/Cart";
 import { Link } from "react-router-dom";
+import OpeningHours from "../components/ui/OpeningHours";
 
 const Cafe = () => {
   const { id } = useParams();
@@ -10,26 +11,32 @@ const Cafe = () => {
   return (
     <div className="bg-white">
       <Container className="py-10">
-        <div className="mb-5">
+        <div className="mb-5 text-gray-500 font-semibold">
           <Link to="/" className="underline underline-offset-2 hover:no-underline">
             Liste des cafés
           </Link>
-          &nbsp;&gt; {id}
+          <span className="px-3">&gt;</span>
+          {id}
         </div>
 
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{id}</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">Description de {id}</p>
-        <div className="my-2 flex items-center gap-x-1.5">
-          <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-            <div className="h-3 w-3 rounded-full bg-emerald-500" />
-          </div>
-          <p className="text-sm leading-5 text-gray-500">Ouvert</p>
-        </div>
         <img
-          className="mt-6 rounded-lg shadow-xl h-80 object-cover"
+          className="mb-6 rounded-lg shadow-xl object-cover w-full md:w-auto md:h-96"
           src="https://i.pinimg.com/originals/8f/5f/d0/8f5fd07d1034e0d4941c4ad9d58ec055.jpg"
           alt=""
         />
+
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{id}</h2>
+        <div className="my-3 flex items-center gap-x-1.5">
+          <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+            <div className="h-3 w-3 rounded-full bg-emerald-500" />
+          </div>
+          <p className="text-sm leading-5 text-gray-500 font-semibold">Ouvert</p>
+        </div>
+        <p className="mt-2 text-lg leading-8 text-gray-600 max-w-3xl">
+          Sandwichs maisons préparés sur place. Succulents bagels au saumon fumé. De namebreux choix végé. Carte de
+          fidélité disponible. Baby-foot. Ouvert l'été.
+        </p>
+        <OpeningHours />
       </Container>
       <Container className="py-10 border-t border-gray-200">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Menu</h2>
