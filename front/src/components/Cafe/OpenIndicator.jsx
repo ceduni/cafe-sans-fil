3866e39b61db9cd14de7f1d489161c6c71189e1c
axeelz <axelzareb@hotmail.fr>
2023-11-09 15:56:58 -0500

    indicateur "fermé" ne "rayonne pas" comme ouvert

--- front/src/components/Cafe/OpenIndicator.jsx
+++ front/src/components/Cafe/OpenIndicator.jsx
@@ -5,7 +5,7 @@ const OpenIndicator = ({ isOpen, size = "sm" }) => {
 
   return (
     <div className={classNames("flex items-center gap-x-1.5", size === "sm" ? "py-3" : "mt-2")}>
-      <div className={classNames("flex-none rounded-full p-1", isOpen ? "bg-emerald-500/20" : "bg-red-500/20")}>
+      <div className={classNames("flex-none rounded-full p-1", isOpen ? "bg-emerald-500/20" : "")}>
         <div className={classNames("h-3 w-3 rounded-full", isOpen ? "bg-emerald-500" : "bg-red-500")} />
       </div>
       <p
