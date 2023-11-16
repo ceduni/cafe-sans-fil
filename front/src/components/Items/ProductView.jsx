54fb5316d9cddaaf05c5e0ba7ccc154600754eb7
axeelz <axelzareb@hotmail.fr>
2023-11-16 14:27:05 -0500

    Hover vue de produit indisponible

--- front/src/components/Items/ProductView.jsx
+++ front/src/components/Items/ProductView.jsx
@@ -138,7 +138,7 @@ const ProductView = ({ item, open, setOpen, onSubmit }) => {
                             text-base font-medium text-white \
                             hover:bg-emerald-700 \
                             focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 \
-                            disabled:opacity-50"
+                            disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-emerald-600"
                             disabled={!item.is_available || isAddingToCart}>
                             {isAddingToCart ? "Ajout en cours..." : "Ajouter au panier"}
                           </button>
