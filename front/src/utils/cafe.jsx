75fa62954d69c72e443efca027e97581c9983379
axeelz <axelzareb@hotmail.fr>
2023-11-16 13:54:10 -0500

    On affiche les messages qui ont pas de start et de end

--- front/src/utils/cafe.jsx
+++ front/src/utils/cafe.jsx
@@ -8,8 +8,10 @@ export const displayCafeLocation = (location) => {
 export const shouldDisplayInfo = (object) => {
   // Prend un objet de type
   // { "type": "string", "value": "string", "start": "string (YYYY-MM-DDTHH:mm:ss.ssssss)", "end": "string (YYYY-MM-DDTHH:mm:ss.ssssss)" }
-  // et retourne true si l'objet est valide et qu'il est dans la période de validité.
+  // et retourne true si on est dans la période de validité.
   if (!object) return false;
+  // Si start ou end n'existent pas, on retourne true
+  if (!object.start || !object.end) return true;
   const now = new Date();
   const start = new Date(object.start);
   const end = new Date(object.end);
