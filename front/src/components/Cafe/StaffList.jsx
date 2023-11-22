7f7ba51d784e83e8a2a0364ceed05e884d1bf018
axeelz <axelzareb@hotmail.fr>
2023-11-22 18:19:03 -0500

    Affichage liste staff réelle

--- front/src/components/Cafe/StaffList.jsx
+++ front/src/components/Cafe/StaffList.jsx
@@ -1,50 +1,42 @@
 import Avatar from "@/components/Avatar";
 import Container from "@/components/Container";
-
-const people = [
-  {
-    name: "Leslie Alexander",
-    email: "leslie.alexander@umontreal.ca",
-    role: "Administrateur",
-  },
-  {
-    name: "Michael Foster",
-    email: "michael.foster@umontreal.ca",
-    role: "Administrateur",
-  },
-  {
-    name: "Dries Vincent",
-    email: "dries.vincent@umontreal.ca",
-    role: "Bénévole",
-  },
-  {
-    name: "Lindsay Walton",
-    email: "lindsay.walton@umontreal.ca",
-    role: "Bénévole",
-  },
-  {
-    name: "Courtney Henry",
-    email: "courtney.henry@umontreal.ca",
-    role: "Bénévole",
-  },
-  {
-    name: "Tom Cook",
-    email: "tom.cook@umontreal.ca",
-    role: "Bénévole",
-  },
-];
+import useApi from "@/hooks/useApi";
+import { useParams, Link } from "react-router-dom";
 
 const StaffList = () => {
+  const { id } = useParams();
+  const cafeSlug = id;
+
+  const [data, isLoading, error] = useApi(`/cafes/${cafeSlug}`);
+
+  if (error) {
+    if (error.status === 422) {
+      throw new Response("Not found", { status: 404, statusText: "Ce café n'existe pas" });
+    }
+    return <EmptyState type="error" error={error} />;
+  }
+
+  // On récupère le staff du café
+  const staff = data?.staff;
+
   return (
     <Container className="py-10">
+      <div className="mb-5 text-gray-500 font-semibold">
+        <Link to={`/cafes/${cafeSlug}`} className="underline underline-offset-2 hover:no-underline">
+          {(isLoading && <span className="animate-pulse">Chargement...</span>) || data?.name}
+        </Link>
+        <span className="px-3">&gt;</span>
+        <span>Staff</span>
+      </div>
+
       <ul role="list" className="divide-y divide-gray-100">
-        {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+        {staff?.map((person) => (
+          <li key={person.username} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
-              <Avatar name={person.name} />
+              <Avatar name={person.username} />
               <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                <p className="text-sm font-semibold leading-6 text-gray-900">{person.username}</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.username}</p>
               </div>
             </div>
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
--- front/src/routes/CafeOrders.jsx
+++ front/src/routes/CafeOrders.jsx
@@ -2,7 +2,7 @@ import Container from "@/components/Container";
 import CafeOrderCard from "@/components/Orders/CafeOrderCard";
 import authenticatedRequest from "@/helpers/authenticatedRequest";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const CafeOrders = () => {
   const { id } = useParams();
@@ -33,6 +33,13 @@ const CafeOrders = () => {
 
   return (
     <Container className="py-10">
+      <div className="mb-5 text-gray-500 font-semibold">
+        <Link to={`/cafes/${cafeSlug}`} className="underline underline-offset-2 hover:no-underline">
+          <span>{cafeSlug}</span>
+        </Link>
+        <span className="px-3">&gt;</span>
+        <span>Commandes</span>
+      </div>
       {isLoading && <p>Chargement...</p>}
       {orders.length === 0 && !isLoading && <p>Aucune commande pour le moment.</p>}
       {orders.length > 0 && orders.map((order) => <CafeOrderCard order={order} key={order.order_id} />)}
