feb53316f89b3e5113f34f261b51136445562e73
axeelz <axelzareb@hotmail.fr>
2023-11-23 13:19:58 -0500

    Cleanup Navbar + sticky + fix affichage bouton se connecter mobile

--- front/src/components/Orders/Cart.jsx
+++ front/src/components/Orders/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = ({ open, setOpen }) => {
 
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+      <Dialog as="div" className="relative z-40" onClose={setOpen}>
         <Transition.Child
           as={Fragment}
           enter="ease-in-out duration-500"
