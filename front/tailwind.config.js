f3bf234912c38700019778e28338b1c527a1a8ae
axeelz <axelzareb@hotmail.fr>
2023-11-28 14:44:10 -0500

    Move animations to tailwind config

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -10,6 +10,20 @@ export default {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
         secondary: ["Rowdies", "sans-serif"],
       },
+      animation: {
+        "animated-text": "slideUpDown 2.1s ease-in-out infinite",
+        "fade-in": "fadeIn 0.2s ease-out",
+      },
+      keyframes: {
+        slideUpDown: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(1.1px)" },
+        },
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(-20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
