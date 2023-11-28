fdda9cca5484e1c7ddbcba861f77eb5ea9a8297f
GokaGokai <gokagokaigokai@gmail.com>
2023-11-28 04:39:03 -0500

    Add full name, email, photo to StaffList

--- front/src/components/Cafe/StaffList.jsx
+++ front/src/components/Cafe/StaffList.jsx
@@ -1,13 +1,31 @@
 import Avatar from "@/components/Avatar";
 import Container from "@/components/Container";
 import useApi from "@/hooks/useApi";
+import React, { useEffect, useState } from 'react';
 import { useParams, Link } from "react-router-dom";
+import { getUserFromUsername } from "@/utils/getFromId";
 
 const StaffList = () => {
   const { id } = useParams();
   const cafeSlug = id;
-
   const [data, isLoading, error] = useApi(`/cafes/${cafeSlug}`);
+  const [staffDetails, setStaffDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchStaffDetails = async () => {
+      const fetchedStaffDetails = await Promise.all(
+        data?.staff.map(async (person) => {
+          const userData = await getUserFromUsername(person.username);
+          return userData ? { ...person, ...userData } : person;
+        }) || []
+      );
+      setStaffDetails(fetchedStaffDetails);
+    };
+
+    if (data?.staff) {
+      fetchStaffDetails();
+    }
+  }, [data?.staff]);
 
   if (error) {
     if (error.status === 422) {
@@ -15,9 +33,6 @@ const StaffList = () => {
     }
     return <EmptyState type="error" error={error} />;
   }
-
-  // On récupère le staff du café
-  const staff = data?.staff;
 
   return (
     <Container className="py-10">
@@ -30,17 +45,17 @@ const StaffList = () => {
       </div>
 
       <ul role="list" className="divide-y divide-gray-100">
-        {staff?.map((person) => (
-          <li key={person.username} className="flex justify-between gap-x-6 py-5">
+        {staffDetails.map((user) => (
+          <li key={user.username} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
-              <Avatar name={person.username} />
+              <Avatar name={user.username} image={user.photo_url} key={user.user_id} />
               <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{person.username}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.username}</p>
+                <p className="text-sm font-semibold leading-6 text-gray-900">{`${user.first_name} ${user.last_name}`}</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{user.email}</p>
               </div>
             </div>
             <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+              <p className="text-sm leading-6 text-gray-900">{user.role}</p>
             </div>
           </li>
         ))}
