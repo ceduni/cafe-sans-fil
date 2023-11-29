be1ddb81e433973fa9409b3d7f0fa16da3fac6e6
axeelz <axelzareb@hotmail.fr>
2023-11-28 22:39:02 -0500

    Fix CafeOrders

--- front/src/routes/CafeOrders.jsx
+++ front/src/routes/CafeOrders.jsx
@@ -8,6 +8,7 @@ import toast from "react-hot-toast";
 import ErrorState from "@/components/ErrorState";
 import useApi from "@/hooks/useApi";
 import { Helmet } from "react-helmet-async";
+import EmptyState from "@/components/EmptyState";
 
 const CafeOrders = () => {
   const { id: cafeSlug } = useParams();
@@ -98,7 +99,7 @@ const CafeOrders = () => {
           <div role="status" className="flex justify-center items-center h-48 w-full text-gray-500 font-semibold">
             <svg
               aria-hidden="true"
-              class="w-8 h-8 text-gray-200 animate-spin fill-emerald-600"
+              className="w-8 h-8 text-gray-200 animate-spin fill-emerald-600"
               viewBox="0 0 100 101"
               fill="none"
               xmlns="http://www.w3.org/2000/svg">
@@ -111,7 +112,7 @@ const CafeOrders = () => {
                 fill="currentFill"
               />
             </svg>
-            <span class="sr-only">Loading...</span>
+            <span className="sr-only">Loading...</span>
           </div>
         )}
         {orders.length === 0 && !areOrdersLoading && <EmptyState name="commande" genre="féminin" />}
