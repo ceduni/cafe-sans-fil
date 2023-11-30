9fccbad9311307ca1630ea13163a53991cbf5b04
axeelz <axelzareb@hotmail.fr>
2023-11-30 13:35:57 -0500

    Improved EditCafe component

---
+++ front/src/hooks/useIsVisible.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export function useIsVisible(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => setIntersecting(entry.isIntersecting));
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
