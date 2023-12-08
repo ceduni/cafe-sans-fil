9872dc5cf84e6f493e1ee880cec6deafdca718d2
axeelz <axelzareb@hotmail.fr>
2023-12-08 13:24:27 -0500

    EditMenu accesible aux membres au lieu des admin

---
+++ front/src/helpers/MemberOnly.jsx
@@ -0,0 +1,30 @@
+import { useAuth } from "@/hooks/useAuth";
+import { isMember } from "@/utils/admin";
+import EmptyState from "@/components/EmptyState";
+import ErrorState from "@/components/ErrorState";
+
+const MemberOnly = ({ children, cafe, error }) => {
+  const { user } = useAuth();
+
+  if (error) {
+    if (error.status === 404) {
+      throw new Response("Not found", { status: 404, statusText: "Ce café n'existe pas" });
+    }
+    return <EmptyState type="error" error={error} />;
+  }
+
+  if (cafe && !isMember(cafe, user?.username)) {
+    return (
+      <ErrorState
+        title="Accès refusé"
+        message="Vous n'avez pas accès à cette page"
+        linkText={`Retour à ${cafe.name}`}
+        linkTo={`/cafes/${cafe.slug}`}
+      />
+    );
+  }
+
+  return children;
+};
+
+export default MemberOnly;
