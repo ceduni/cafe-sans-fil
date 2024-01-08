a04b4a870e5f7e22faaa2562afae12f5bc5b25a6
Louis-Edouard LAFONTANT <lelafontant@yahoo.ca>
2024-01-08 12:45:31 -0500

    astro config fix

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://ceduni.github.io',
-    base: '/cafe-sans-fil/docs',
-});
+export default defineConfig();
