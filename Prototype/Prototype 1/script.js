2ee40f9b699ef7d0994e2c5e1c87d5887f416f3e
Flo_JB <eedfloryjb06@gmail.com>
2024-02-13 00:26:41 -0500

    ignorer background color

--- Prototype/Prototype 1/script.js
+++ Prototype/Prototype 1/script.js
@@ -89,9 +89,32 @@ function setupMenu(app, menu) {
 const app = document.querySelector("#app");
 app.innerHTML = `
   <div class=content-wrapper>
-    <h1>Menu</h1>
+    <h3>Menu</h3>
     <div id="menu" class="menu"></div>
   </div>
 `;
 
 setupMenu(app, document.querySelector("#menu"));
+
+// evenement et publications
+
+$(document).ready(function () {
+  //  showing the clicked tab
+  $('.tabs-control a').click(function () {
+    $('.tabs-control a').removeClass('active');
+    $(this).addClass('active');
+
+    let currentTab = $(this).attr('href');
+    $('.box').hide();
+    $(currentTab).fadeIn();
+
+    // saving current tab to local storage
+    let index = $(this).index();
+    localStorage.setItem('currentTab', index);
+  });
+
+  //  getting last current tab from storage
+  let getTab = localStorage.getItem('currentTab');
+  $('.tabs-control a').eq(getTab).addClass('active');
+  $('.box').eq(getTab).show();
+});
