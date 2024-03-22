6f62f61182237994c2ec232826fc5d5b9b2579b4
Flo_JB <eedfloryjb06@gmail.com>
2024-03-21 22:41:17 -0400

    search service

---
+++ back/app/services/search_service.py
@@ -0,0 +1,14 @@
+from typing import List
+from app.models.cafe_model import Cafe, MenuItem  
+
+@staticmethod
+async def search(query: str) -> List[Cafe]:
+    regex_pattern = f".*{query}.*"
+    regex_options = 'i'  
+    
+    # Recherche des cafés dont les items de menu correspondent à la requête
+    matching_cafes = await Cafe.find(
+        Cafe.menu_items.any(MenuItem.name.match(regex_pattern, options=regex_options))
+    ).to_list()
+    
+    return matching_cafes
