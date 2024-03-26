888c3859a400d189303b56c49df34b8b31e1f9e9
Flo_JB <eedfloryjb06@gmail.com>
2024-03-25 20:13:15 -0400

    modification du search

--- back/app/services/search_service.py
+++ back/app/services/search_service.py
@@ -15,16 +15,14 @@
     
 #     return matching_cafes
 
-
-
-# ###Verson 2  fonctionne avec items  seulement 
+###Version 2 , fonctionne avec items et cafes 
 # from app.models.cafe_model import Cafe
 # from typing import List, Dict, Any
 
 # async def search(query: str, **filters) -> Dict[str, List[Any]]:
 #     regex_pattern = {"$regex": query, "$options": "i"}
-    
-#     # plus ou moins le modele de cafe_service
+
+# # plus ou moins le modele de cafe_service
 #     for key in ['is_open', 'in_stock']:
 #         if key in filters:
 #             if filters[key].lower() == 'true':
@@ -32,23 +30,24 @@
 #             elif filters[key].lower() == 'false':
 #                 filters[key] = False
 
-#     # recherche dans les éléments de menu
-#     item_query = {"menu_items": {"$elemMatch": {"name": regex_pattern}}}
-#     item_query.update(filters)  
-#     matching_cafes = await Cafe.find(item_query).to_list()
+#     # Combinaison des search pour inclure les cafés par leur nom et par les éléments du menu
+#     combined_query = {
+#         "$or": [
+#             {"name": regex_pattern},  # search par nom du café
+#             {"menu_items": {"$elemMatch": {"name": regex_pattern}}}  # search dans les éléments de menu
+#         ]
+#     }
+#     combined_query.update(filters)  
+#     matching_cafes = await Cafe.find(combined_query).to_list()
 
-#    # uniquement les cafés contenant des éléments de menu correspondants
-#     return {"matching_cafesssssss": matching_cafes}
+#     return {"matching_cafessss_and_items": matching_cafes}
 
-
-###Version 3 , fonctionne avec items et cafes 
 from app.models.cafe_model import Cafe
 from typing import List, Dict, Any
 
 async def search(query: str, **filters) -> Dict[str, List[Any]]:
     regex_pattern = {"$regex": query, "$options": "i"}
 
-# plus ou moins le modele de cafe_service
     for key in ['is_open', 'in_stock']:
         if key in filters:
             if filters[key].lower() == 'true':
@@ -56,14 +55,40 @@ async def search(query: str, **filters) -> Dict[str, List[Any]]:
             elif filters[key].lower() == 'false':
                 filters[key] = False
 
-    # Combinaison des search pour inclure les cafés par leur nom et par les éléments du menu
+   # Combinaison des search pour inclure les cafés par leur nom et par les éléments du menu
     combined_query = {
         "$or": [
-            {"name": regex_pattern},  # search par nom du café
-            {"menu_items": {"$elemMatch": {"name": regex_pattern}}}  # search dans les éléments de menu
+            {"name": regex_pattern},  
+            {"menu_items": {"$elemMatch": {"name": regex_pattern}}} 
         ]
     }
-    combined_query.update(filters)  
-    matching_cafes = await Cafe.find(combined_query).to_list()
+    combined_query.update(filters)
+    matching_cafes_full = await Cafe.find(combined_query).to_list()
 
-    return {"matching_cafessss_and_items": matching_cafes}
+
+    matching_cafes_and_items = []
+    for cafe in matching_cafes_full:
+        filtered_menu_items = [item for item in cafe.menu_items if query.lower() in item.name.lower()]
+
+        
+        cafe_dict = {
+            "_id": str(cafe.id), 
+            "cafe_id": str(cafe.cafe_id),
+            "name": cafe.name,
+            "slug": cafe.slug,
+            "description": cafe.description,
+            "image_url": cafe.image_url,
+            "faculty": cafe.faculty,
+            "is_open": cafe.is_open,
+            "status_message": cafe.status_message,
+            "opening_hours": cafe.opening_hours,
+            "location": cafe.location,
+            "contact": cafe.contact,
+            "social_media": cafe.social_media,
+            "payment_methods": cafe.payment_methods,
+            "additional_info": cafe.additional_info,
+            "menu_items": filtered_menu_items  
+        }
+        matching_cafes_and_items.append(cafe_dict)
+
+    return {"matching_cafesssss_and_items": matching_cafes_and_items}
