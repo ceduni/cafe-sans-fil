5c81621d9c0207fdefaaa6074cad0d8002ea1eea
Flo_JB <eedfloryjb06@gmail.com>
2024-04-02 20:25:23 -0400

    prop

--- front/src/components/Search/SearchResults.jsx
+++ front/src/components/Search/SearchResults.jsx
@@ -54,18 +54,18 @@ const SearchResults = ({ searchQuery }) => {
   const { data: searchResults} = useApi(`/search?query=${searchQuery}`);
 
 
-  if (!searchResults || searchResults.matching_cafesssss_and_items.length === 0) {
+  if (!searchResults || searchResults.matching_cafes_and_their_items.length === 0) {
     return <EmptyState name="café" />;
   }
   
  
-  const finalResults = searchResults.matching_cafesssss_and_items; //Array des resultats
+  const finalResults = searchResults.matching_cafes_and_their_items; //Array des resultats
 
   return (
     <>
       <div className="relative top-1 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-8">
         {finalResults.map((result) => (
-          <CafeCard key={result.slug || result.item_id} cafe={result} />
+          <CafeCard key={result.slug || result.item_id} cafe={result} searchQuery={searchQuery} />
         ))}
       </div>
     </>
