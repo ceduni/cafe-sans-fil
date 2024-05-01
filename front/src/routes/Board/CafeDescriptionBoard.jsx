762c5b95726d6377315796bf8ec6ee15c9a0d79f
Flo_JB <eedfloryjb06@gmail.com>
2024-05-01 15:14:07 -0400

    photo

--- front/src/routes/Board/CafeDescriptionBoard.jsx
+++ front/src/routes/Board/CafeDescriptionBoard.jsx
@@ -12,14 +12,14 @@ const CafeDescriptionBoard = ({ cafe }) => {
   const additionalVolunteers = cafe.volunteers - cafe.staff.length;
   
   return (
-    <div className="bg-white shadow rounded-lg p-4 mb-4">
+    <div className="bg-black shadow rounded-lg p-4 mb-4">
       <div className="mb-4">
-        <h2 className="text-xl font-bold text-gray-900">{cafe.name}</h2>
-        <p className="text-xs text-gray-600">{cafe.location}</p>
-        <p className="text-sm text-gray-600 pt-2">{cafe.description}</p>
+        <h2 className="text-xl font-bold text-white">{cafe.name}</h2>
+        <p className="text-xs text-white">{cafe.location}</p>
+        <p className="text-sm text-white pt-2">{cafe.description}</p>
         
       </div>
-      <div className="mb-4">
+      <div className="mb-4 text-white">
         <strong>Appareils:</strong>
         <ul className="list-disc list-inside text-sm">
           {cafe.appareils.map((appareil, index) => (
@@ -28,7 +28,7 @@ const CafeDescriptionBoard = ({ cafe }) => {
         </ul>
       </div>
       <div>
-      <div className="flex  items-center mb-4">
+      <div className="flex  items-center text-white mb-4">
         <strong>Staff</strong>
         <button
           onClick={handleEmailClick}className=" hover:text-blue-800" >
@@ -37,21 +37,17 @@ const CafeDescriptionBoard = ({ cafe }) => {
         </button>
       </div>
       <div className="flex flex-wrap items-center">
-  {cafe.staff.slice(0, 2).map((member, index) => (
-    <div key={index} className="flex items-center mr-4 mb-4">
-      {member.image ? (
-        <img
-          src={member.image}
-          alt={member.name}
-          className="h-10 w-10 rounded-full mr-2"
-        />
-      ) : (
-        <div className="h-10 w-10 rounded-full bg-gray-200 mr-2"></div>
-      )}
-      <span className="text-sm mr-2">{member.name}</span>
-    </div>
-  ))}
-</div>
+          {cafe.staff.map((member, index) => (
+            <div key={index} className="flex items-center mr-4 mb-4">
+              <img
+                src={member.photoUrl}
+                alt={`${member.firstName} ${member.lastName}`}
+                className="h-10 w-10 rounded-full mr-2"
+              />
+              <span className="text-sm mr-2">{`${member.firstName} ${member.lastName}`}</span>
+            </div>
+          ))}
+        </div>
       </div>
       <div className="text-right mt-4">
         <button className="text-sm text-blue-600 border border-blue-600 rounded py-1 px-3 hover:bg-blue-600 hover:text-white transition-colors duration-300">
@@ -63,3 +59,4 @@ const CafeDescriptionBoard = ({ cafe }) => {
 };
 
 export default CafeDescriptionBoard;
+
