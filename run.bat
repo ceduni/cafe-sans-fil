7222001d5a909636b28f9a08c1589c11631a9937
gonxped31 <gonxped31@gmail.com>
2024-06-21 15:53:42 -0400

    added bat files and .env

---
+++ run.bat
@@ -0,0 +1,19 @@
+@echo off
+
+echo Starting FastAPI backend
+cd back
+start pipenv run uvicorn app.main:app --reload
+cd ..
+
+echo Starting React frontend
+cd front
+start npm run dev
+cd ..
+
+timeout /t 6 >nul
+
+echo Opening in browser
+start http://127.0.0.1:8000/docs#/
+start http://localhost:5173/
+
+echo Done
