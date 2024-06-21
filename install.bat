65fe2a6a9c31489139e27ebe355226916fa4b053
GokaGokai <gokagokaigokai@gmail.com>
2024-06-21 13:23:36 -0400

    Add `pip install pipenv` to `install.bat`

--- install.bat
+++ install.bat
@@ -1,5 +1,11 @@
 @echo off
 
+pip show pipenv >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pipenv
+    pip install pipenv
+)
+
 echo Installing backend dependencies
 cd back
 call pipenv install
