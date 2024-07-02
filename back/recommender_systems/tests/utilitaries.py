d187b82633d28d4a0f1d745ab1690584a6651e38
gonxped31 <gonxped31@gmail.com>
2024-07-02 18:45:07 -0400

    add utilitaries tests

--- back/recommender_systems/tests/utilitaries.py
+++ back/recommender_systems/tests/utilitaries.py
@@ -1,27 +1,146 @@
-from utils import utilitaries as Utilitaries
-from app.models.user_model import User
-from app.models.cafe_model import Cafe, MenuItem
-from typing import List
+from recommender_systems.utils import utilitaries as Utilitaries
 
-def tes_reshape():
-    A: list[str] = ['This', 'Is']
-    B: list[str] = ['A', 'Test']
-    C: list[str] = ['For', 'This', 'Code']
+def test_jaccard_similarity():
+    A = set(['A', 'B', 'C'])
+    B = set(['B', 'C', 'D'])
+    C = set(['C', 'D', 'E'])
 
-    assert Utilitaries.reshape(A,B) == (A, B)
-    assert Utilitaries.reshape(B,C) == (['A', 'Test', '0'], C)
-    assert Utilitaries.reshape(C,A) == (C, ['This', 'Is', '0'])
+    assert Utilitaries.jaccard_similarity(A, B) == 0.5
+    assert Utilitaries.jaccard_similarity(A, C) == 0.2
+    assert Utilitaries.jaccard_similarity(A, A) == 1
+    assert Utilitaries.jaccard_similarity(B, C) == 0.5
 
-def test_items_slugs(items: List[MenuItem]):
-    slugs: list[str] = []
-    for item in items:
-        slugs.append(item.slug)
+def test_users_similarity():
+    u_list = [['A', 'B', 'C'], ['B', 'C', 'D'], ['C', 'D', 'E']]
+    v_list = [['A', 'B', 'C'], ['B', 'C', 'D'], ['C', 'D', 'E']]
 
-    assert Utilitaries.items_slugs([items[0]])[0] == slugs[0] # Check for one item
-    assert Utilitaries.items_slugs(items) == slugs # Check for many items
+    u_list_2 = [['A', 'B', 'C'], ['B', 'C', 'D'], ['C', 'D', 'E']]
+    v_list_2 = [['E', 'D', 'G'], ['G', 'H', 'I'], ['A', 'B', 'H']]
 
-def test_list_items(orders_ids: List[str], action: str):
+    u_list_3 = [['A', 'B', 'C'], ['B', 'C', 'D'], ['C', 'D', 'E']]
+    v_list_3 = [['D', 'B', 'E'], ['A', 'C', 'E'], ['C', 'F', 'G']]
+
+    assert round(Utilitaries.users_similarity(u_list, v_list), 1) == 3
+    assert round(Utilitaries.users_similarity(u_list_2, v_list_2), 1) == 0
+    assert round(Utilitaries.users_similarity(u_list_3, v_list_3), 1) == 0.6
+
+def test_sort_items_by_occurences():
+    map = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+    assert Utilitaries._sort_items_by_occurences(map) == ['d', 'c', 'b', 'a']
+
+def test_most_bought_items():
+    orders = [
+        {'items': [{'item_slug': 'a'}, {'item_slug': 'b'}, {'item_slug': 'c'}]},
+        {'items': [{'item_slug': 'b'}, {'item_slug': 'c'}, {'item_slug': 'd'}]},
+        {'items': [{'item_slug': 'c'}, {'item_slug': 'd'}, {'item_slug': 'e'}]},
+    ]
+
+    assert Utilitaries.most_bought_items(orders) == ['c', 'd', 'b', 'e', 'a']
+
+def test_most_liked_items():
+    items = [
+        {'slug': 'a', 'likes': ['1', '2', '3', '4', '5', '6']},
+        {'slug': 'b', 'likes': ['1', '2', '3', '4', '5', '6', '7']},
+        {'slug': 'c', 'likes': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']},
+        {'slug': 'd', 'likes': ['1', '2', '3', '4', '5', '6', '7', '8', '9']},
+    ]
+
+    assert Utilitaries.most_liked_items(items) == ['c', 'd', 'b', 'a']
+    assert Utilitaries.most_liked_items(items, 2) == ['c', 'd']
+
+#TODO
+def test_list_items():
+    orders_id = [
+        "9d22e803-d98a-4071-ae48-49c35a13dd53", # acqui-de-droit, order: 1
+        "454dc4c9-8641-49c3-8c7a-d85323920900", # tore-et-fraction, order: 2
+    ]
+    assert Utilitaries.list_items(orders_id) == [
+        "espresso", "jus-v8", "espresso-double", "latte-ou-cappucino", "gomme"
+    ]
+
+def test_regroup_by_cluster():
+    items = [
+        {'slug': 'a', 'cluster': '0'},
+        {'slug': 'b', 'cluster': '1'},
+        {'slug': 'c', 'cluster': '0'},
+        {'slug': 'd', 'cluster': '3'},
+        {'slug': 'h', 'cluster': '1'},
+        {'slug': 'g', 'cluster': '2'},
+        {'slug': 'f', 'cluster': '3'},
+        {'slug': 'z', 'cluster': '4'},
+    ]
+
+    assert Utilitaries.regroup_by_cluster(items) == {'0': ['a', 'c'], '1': ['b', 'h'], '2': ['g'], '3': ['d', 'f'], '4': ['z']}
+
+#TODO
+def test_filter_items_by_cafe():
+    items = [
+        "le-mozza",
+        "le-marbre",
+        "espresso",
+        "espresso-double",
+    ]
+
+    cafe_slug_1 = "acquis-de-droit"
+    cafe_slug_2 = "cafcom"
+    #assert Utilitaries.filter_items_by_cafe(items, cafe_slug_1) == ["le-mozza", "le-marbre", "espresso", "espresso-double"]
+    #assert Utilitaries.filter_items_by_cafe(items, cafe_slug_2) == ["le-mozza", "le-marbre", "espresso", "espresso-double"]
     pass
 
-def test_meal_not_consumed(cafe: Cafe, user: User):
+#TODO
+def test_meal_not_consumed():
     pass
+
+#TODO
+def test_find_cafe_by_item():
+    pass
+
+def test_find_indexes():
+    items = ['a', 'b', 'a', 'c', 'd', 'd', 'e', 'd']
+
+    assert Utilitaries.find_indexes(items, 'a') == [0, 2]
+    assert Utilitaries.find_indexes(items, 'b') == [1]
+    assert Utilitaries.find_indexes(items, 'c') == [3]
+    assert Utilitaries.find_indexes(items, 'd') == [4, 5, 7]
+    assert Utilitaries.find_indexes(items, 'e') == [6]
+
+def test_health_score():
+    item = {
+        'nutritional_informations': {
+            'calories': 10,
+            'protein': 100,
+            'carbohydrates': 2,
+            'lipid': 3,
+            'saturated_fat': 4,
+            'sodium': 5,
+            'sugar': 0,
+            'fiber': 700,
+            'percentage_fruit_vegetables_nuts': 800,
+            'vitamins': 900
+        }
+    }
+
+    assert Utilitaries.health_score(item) == -6
+
+def test_reshape():
+    A = [1, 2, 3, 4, 5, 6]
+    B = [7, 5, 9]
+    assert Utilitaries.reshape(A, B) == (A, [7, 5, 9, '0', '0', '0'])
+
+if __name__ == "__main__":
+    print("Running tests...")
+    test_jaccard_similarity()
+    test_users_similarity()
+    test_sort_items_by_occurences()
+    test_most_bought_items()
+    test_most_liked_items()
+    test_list_items()
+    test_regroup_by_cluster()
+    test_filter_items_by_cafe()
+    test_meal_not_consumed()
+    test_find_cafe_by_item()
+    test_find_indexes()
+    test_health_score()
+    test_reshape()
+    print("All tests passed!")
