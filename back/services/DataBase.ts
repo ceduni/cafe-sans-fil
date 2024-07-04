eb19ef44699d0ffd69f30fa10aa73df4d840886d
larry fotso <fotsoguiffo8@gmail.com>
2024-07-04 16:19:18 -0400

    API -- getstock bug fixed

--- back/services/DataBase.ts
+++ back/services/DataBase.ts
@@ -1,36 +1,31 @@
 import { MongoClient, Db, Collection, Document } from 'mongodb';
-
+import mongoose,{Connection} from 'mongoose';
 class Database {
     private static instance: Database;
-    private client: MongoClient;
-    private db: Db;
-    private static readonly  URI = "dodo";
+    private client: Connection;
+    //private db: Db;
+    private static readonly  URI = "mongodb+srv://cafe:sans-fil@cluster0.ti5co91.mongodb.net/sales?retryWrites=true&w=majority";
     private static _connectionFlag:boolean=false;
     private constructor(dbName: string) {
-        this.client = new MongoClient(Database.URI);
-        this.db = this.client.db(dbName);
+        this.client = mongoose.connection;
     }
 
     public static async getInstance(dbName: string): Promise<Database> {
-        console.log("fect");
+
         if (!Database.instance) {
-            console.log('Creating new Database instance...');
             Database.instance = new Database(dbName);
             await Database.instance.connect();
         }
         if(!Database._connectionFlag){
-            console.log("not connected");
             await Database.instance.connect();
         }
-        console.log('returning');
         return Database.instance;
     }
 
     public async connect() {
         
             try {
-                console.log("asking for connection")
-                await this.client.connect();       
+                await mongoose.connect(Database.URI);    
                 Database._connectionFlag = true;
                 
                 console.log('Connected to MongoDB');
@@ -42,17 +37,12 @@ class Database {
         
     }
 
-    public getCollection<T extends Document>(name: string): Collection<T> {
-        if (!Database._connectionFlag) {
-            throw new Error('Client is not connected');
-        }
-        return this.db.collection<T>(name);
-    }
+   
 
     public async close() {
         if (Database._connectionFlag) {
             try {
-                await this.client.close();
+                await mongoose.disconnect();
                 Database._connectionFlag = false;
                 console.log('Disconnected from MongoDB');
             } catch (error) {
---
+++ back/src/models/StockModel.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IStock extends Document {
+    item_name: string;
+    category: string;
+    quantity: number;
+}
+
+const StockSchema: Schema = new Schema({
+    item_name: { type: String, required: true },
+    category: { type: String, required: true },
+    quantity: { type: Number, required: true }
+});
+
+const Stock = mongoose.model<IStock>('Stocks', StockSchema);
+
+export { IStock, Stock };
+export default Stock;
