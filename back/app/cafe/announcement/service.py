7ce8c381889558f5737382ded7244a3d7bcb943b
GokaGokai <gokagokaigokai@gmail.com>
2025-03-04 11:35:31 -0500

    Add `@staticmethod` and `@overload` in Announcement/Event Services

--- back/app/cafe/announcement/service.py
+++ back/app/cafe/announcement/service.py
@@ -2,7 +2,7 @@
 Module for handling announcement-related operations.
 """
 
-from typing import List, Union
+from typing import List, Literal, Union, overload
 
 from beanie import PydanticObjectId
 from beanie.odm.queries.find import FindMany
@@ -21,17 +21,56 @@ from app.user.models import User
 class AnnouncementService:
     """Service class for announcement operations."""
 
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[True] = True,
+        as_view: Literal[False] = False,
+        **filters: dict,
+    ) -> List[Announcement]: ...
+
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[False],
+        as_view: Literal[False] = False,
+        **filters: dict,
+    ) -> FindMany[Announcement]: ...
+
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[True],
+        as_view: Literal[True],
+        **filters: dict,
+    ) -> List[AnnouncementView]: ...
+
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[False],
+        as_view: Literal[True],
+        **filters: dict,
+    ) -> FindMany[AnnouncementView]: ...
+
+    @staticmethod
     async def get_all(
         to_list: bool = True,
         as_view: bool = False,
         **filters: dict,
-    ) -> Union[FindMany[Announcement], List[Announcement]]:
+    ) -> (
+        List[Announcement]
+        | FindMany[Announcement]
+        | List[AnnouncementView]
+        | FindMany[AnnouncementView]
+    ):
         """Get announcements."""
         announcement_class = AnnouncementView if as_view else Announcement
         sort_by = filters.pop("sort_by", "-updated_at")
         query = announcement_class.find(filters).sort(sort_by)
         return await query.to_list() if to_list else query
 
+    @staticmethod
     async def get(
         id: PydanticObjectId,
         as_view: bool = False,
@@ -41,6 +80,7 @@ class AnnouncementService:
         id_field = "id" if as_view else "_id"
         return await announcement_class.find_one({id_field: id})
 
+    @staticmethod
     async def get_by_id_and_cafe_id(
         id: PydanticObjectId,
         cafe_id: PydanticObjectId,
@@ -51,6 +91,7 @@ class AnnouncementService:
         id_field = "id" if as_view else "_id"
         return await announcement_class.find_one({id_field: id, "cafe_id": cafe_id})
 
+    @staticmethod
     async def create(
         current_user: User,
         cafe: Cafe,
@@ -63,6 +104,7 @@ class AnnouncementService:
         await announcement.insert()
         return announcement
 
+    @staticmethod
     async def update(
         announcement: Announcement,
         data: AnnouncementUpdate,
@@ -72,6 +114,7 @@ class AnnouncementService:
         await announcement.save()
         return announcement
 
+    @staticmethod
     async def delete(announcement: Announcement) -> None:
         """Delete an announcement."""
         await announcement.delete()
--- back/app/cafe/event/service.py
+++ back/app/cafe/event/service.py
@@ -2,7 +2,7 @@
 Module for handling event-related operations.
 """
 
-from typing import List, Union
+from typing import List, Literal, Union, overload
 
 from beanie import PydanticObjectId
 from beanie.odm.queries.find import FindMany
@@ -16,17 +16,51 @@ from app.user.models import User
 class EventService:
     """Service class for Event operations."""
 
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[True] = True,
+        as_view: Literal[False] = False,
+        **filters: dict,
+    ) -> List[Event]: ...
+
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[False],
+        as_view: Literal[False] = False,
+        **filters: dict,
+    ) -> FindMany[Event]: ...
+
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[True],
+        as_view: Literal[True],
+        **filters: dict,
+    ) -> List[EventView]: ...
+
+    @overload
+    @staticmethod
+    async def get_all(
+        to_list: Literal[False],
+        as_view: Literal[True],
+        **filters: dict,
+    ) -> FindMany[EventView]: ...
+
+    @staticmethod
     async def get_all(
         to_list: bool = True,
         as_view: bool = False,
         **filters: dict,
-    ) -> Union[FindMany[Event], List[Event]]:
+    ) -> List[Event] | FindMany[Event] | List[EventView] | FindMany[EventView]:
         """Get events."""
         event_class = EventView if as_view else Event
         sort_by = filters.pop("sort_by", "-start_date")
         query = event_class.find(filters).sort(sort_by)
         return await query.to_list() if to_list else query
 
+    @staticmethod
     async def get(
         id: PydanticObjectId,
         as_view: bool = False,
@@ -36,6 +70,7 @@ class EventService:
         id_field = "id" if as_view else "_id"
         return await event_class.find_one({id_field: id})
 
+    @staticmethod
     async def get_by_id_and_cafe_id(
         id: PydanticObjectId,
         cafe_id: PydanticObjectId,
@@ -46,6 +81,7 @@ class EventService:
         id_field = "id" if as_view else "_id"
         return await event_class.find_one({id_field: id, "cafe_id": cafe_id})
 
+    @staticmethod
     async def create(
         current_user: User,
         cafe: Cafe,
@@ -56,6 +92,7 @@ class EventService:
         await event.insert()
         return event
 
+    @staticmethod
     async def update(
         event: Event,
         data: EventUpdate,
@@ -65,6 +102,7 @@ class EventService:
         await event.save()
         return event
 
+    @staticmethod
     async def delete(event: Event) -> None:
         """Delete an event."""
         await event.delete()
