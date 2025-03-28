1c8fe07d02dcf4f075dc13d2890c9440d59977a5
matteo.cm426 <matteo.caron-mondestin@umontreal.ca>
2025-03-28 10:00:28 -0400

    update to event.js: match model with database attributes

--- front/src/models/event.js
+++ front/src/models/event.js
@@ -1,12 +1,37 @@
 export class Event {
     constructor(data) {
-        this.id = data.cafe_id;
+        this.id = data.id;
         this.name = data.name;
-        this.slug = data.slug;
+        this.creator = data.creator.id;
+        this.cafes = data.cafes.map(cafe => cafe.id);        
         this.description = data.description;
-        this.image = data.image_url;
+        this.image_url = data.image_url;
+        this.start_date = data.start_date;
+        this.end_date = data.end_date;
+        this.location = data.location;
+        this.interactions = new Interactions(data.interactions);
+        
+        //these are undefined
+        this.slug = data.slug;
         this.closed = !data.is_open;
         this.status = data.status_message;
         this.hours = data.faculty;
+    }
+}
+
+class Interactions {
+    constructor(data) {
+        data.map((interaction) => {
+            switch(interaction.type){
+                case "ATTEND":
+                    this.attend = {count: interaction.count, me: interaction.me}
+                    break;
+                case "LIKE":
+                    this.likes = {count: interaction.count, me: interaction.me};
+                    break;
+                case "SUPPORT":
+                    this.support = {count: interaction.count, me: interaction.me};
+            }
+        })      
     }
 }
